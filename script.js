05d1364c3b14bdecb6f180ac3f55eabd5f98bd11
Arvexion <288262116+Arvexion@users.noreply.github.com>
2026-06-09 14:18:04 +0800

    Add function to return rock, paper, scissors
    To equally return a random selection from those three strings

--- script.js
+++ script.js
@@ -1,10 +1,29 @@
 
 // Function that randomly returns "rock", "paper", or "scissors"
+function getComputerChoice() {
     // SET randomNumber TO random number between 1 and 3
-    
+    const randomNumber = Math.floor(Math.random() * 3) + 1;
+
     // IF randomNumber IS 1 THEN
-        // RETURN "rock"
+    if (randomNumber === 1) {
+         // RETURN "rock"
+        return "rock";
+        console.log("rock");
+    }
+       
     // IF randomNumber IS 2 THEN
+    if (randomNumber === 2) {
         // RETURN "paper"
+        return "paper";
+        console.log("paper")
+    }
+        
     // IF randomNumber IS 3 THEN
+    if (randomNumber === 3) {
         // RETURN "scissors"
+        return "scissors";
+        console.log("scissors");
+    }
+}
+
+console.log(getComputerChoice());
