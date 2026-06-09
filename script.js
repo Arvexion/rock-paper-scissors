23b47fa60e5b077135dfc60c3443e80af42e78bb
Arvexion <288262116+Arvexion@users.noreply.github.com>
2026-06-09 14:03:42 +0800

    Add pseudocode for the function

---
+++ script.js
@@ -0,0 +1,10 @@
+
+// Function that randomly returns "rock", "paper", or "scissors"
+    // SET randomNumber TO random number between 1 and 3
+    
+    // IF randomNumber IS 1 THEN
+        // RETURN "rock"
+    // IF randomNumber IS 2 THEN
+        // RETURN "paper"
+    // IF randomNumber IS 3 THEN
+        // RETURN "scissors"
