10a089eb844cbe918a7766f154d4a773c2b20a42
Arvexion <288262116+Arvexion@users.noreply.github.com>
2026-06-09 15:08:11 +0800

    Add function to get user input and return it

--- script.js
+++ script.js
@@ -27,13 +27,31 @@ function getComputerChoice() {
 }
 
 // Function that gets user input and returns a valid choice 
+function getHumanChoice() {
     // SET userInput TO user input with prompt
+    let userInput = prompt("CHOOSE: (rock) || (paper) || (scissors)")
 
     // IF userInput IS rock THEN
+    if (userInput === "rock") {
         // RETURN "rock"
+        console.log("rock");
+        return "rock";
+    }
         
     // IF userInput IS paper THEN
+    if (userInput === "paper") {
         // RETURN "paper"
+        console.log("paper")
+        return "paper";
+    }
         
     // IF userInput IS scissors THEN
+    if (userInput === "scissors") {
         // RETURN "scissors"
+        console.log("scissors");
+        return "scissors";
+    }
+}
+    
+
+console.log(getHumanChoice());
